23b6f3530e571089948d8e316ff8ae6f4465c625
manuelledezma687 <manuelledezma687@gmail.com>
2023-02-17 00:57:21 -0300

    second commit

--- test_ratings.py
+++ test_ratings.py
@@ -1,3 +1,4 @@
+from json import dumps
 import requests
 from assertpy.assertpy import assert_that
 
@@ -10,3 +11,20 @@ def test_get_a_rating():
     response = requests.get("http://127.0.0.1:8000/ratings", timeout=10).json()
     first_name = [rating['first_name'] for rating in response]
     assert_that(first_name).contains('Jose')
+    
+def test_create_a_rating():
+    payload = dumps({
+        "rating_id":1,
+        "booking_id":1,
+        "stars":5,
+        "first_name":"Jose",
+        "last_name":"Otero",
+        "comments":"Hi it is a excelent service.",
+        "created_at":"2023-02-16T18:31:41.991612"
+        })
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+    response = requests.post("http://127.0.0.1:8000/ratings", data=payload, headers=headers, timeout=10)
+    assert_that(response.status_code).is_equal_to(requests.codes.created)
